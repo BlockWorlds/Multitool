db0a55d1c09641f76f16da65a8bd81389a48a7dd
ShiniOfTheGami <ctbuschmann@gmail.com>
2015-06-21 02:29:19 +0200

    Fix errors

--- src/main/java/com/theblockworlds/multitool/tools/Jackhammer.java
+++ src/main/java/com/theblockworlds/multitool/tools/Jackhammer.java
@@ -1,5 +1,7 @@
 package com.theblockworlds.multitool.tools;
 
+import java.util.Arrays;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -7,6 +9,7 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import com.theblockworlds.multitool.Multitool;
 import com.theblockworlds.multitool.base.Tool;
@@ -25,15 +28,25 @@ public class Jackhammer extends Tool {
 
 	@Override
 	public void onUse(Block targetBlock, BlockFace face, ItemStack itemUsed, Player player, Action action) {
-		preformAction(targetBlock, face, itemUsed, player, action);
+		performAction(targetBlock, face, itemUsed, player, action);
 	}
 
 	@Override
 	public void onRangedUse(Block targetBlock, BlockFace face, ItemStack itemUsed, Player player, Action action) {
-		preformAction(targetBlock, face, itemUsed, player, action);
+		performAction(targetBlock, face, itemUsed, player, action);
 	}
 	
-	private void preformAction(Block targetBlock, BlockFace face, ItemStack itemUsed, Player player, Action action) {
+	@Override
+	public ItemStack getItemStack(){
+		ItemStack items = new ItemStack(this.getMaterial(), 1, (short) -1);
+		ItemMeta meta = items.getItemMeta();
+		meta.setDisplayName(this.getName());
+		meta.setLore(Arrays.asList("Left Click to remove","Right Click to nophysics remove"));
+		items.setItemMeta(meta);
+		return items;
+	}
+	
+	private void performAction(Block targetBlock, BlockFace face, ItemStack itemUsed, Player player, Action action) {
 		if (action == Action.LEFT_CLICK_AIR || action == Action.LEFT_CLICK_BLOCK) {
 			if (!callEventSuccess(targetBlock, player, false)) {
 				return;
@@ -50,9 +63,9 @@ public class Jackhammer extends Tool {
 	
 	
 	private boolean callEventSuccess(Block targetBlock, Player player, boolean noPhysics) {
-		MultiToolDestroyEvent destoryEvent = new MultiToolDestroyEvent(targetBlock, player, noPhysics);
-		Bukkit.getPluginManager().callEvent(destoryEvent);
-        if (destoryEvent.isCancelled()) {
+		MultiToolDestroyEvent destroyEvent = new MultiToolDestroyEvent(targetBlock, player, noPhysics);
+		Bukkit.getPluginManager().callEvent(destroyEvent);
+        if (destroyEvent.isCancelled()) {
             return false;
         }
         return true;
